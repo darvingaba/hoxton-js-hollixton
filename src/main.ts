2166c249f6b5a5170f7313ce602de63bf3e7e1c8
darvingaba <darvingaba1@gmail.com>
2022-07-13 17:15:40 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -50,9 +50,8 @@ function createProduct(product: Store) {
   productContainer.classList.add("product-container");
 
   productContainer.addEventListener("click", () => {
+    products.innerHTML = "";
     singleProductBuy(product);
-    location.href = "/product.html";
-    console.log(product.id);
   });
   //       <img src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1" alt="">
   let productImage = document.createElement("img");
@@ -75,7 +74,7 @@ function createProduct(product: Store) {
 }
 // ! function to create a product when user buys one
 function singleProductBuy(product: Store) {
-  let container = document.querySelector(".single-product-container");
+  let container = document.querySelector(".products");
   if (container == null) return;
   // <div class="single-product">
   //         <img class="buy-img-product" src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1" alt="">
@@ -86,6 +85,7 @@ function singleProductBuy(product: Store) {
   //             <button class="add-to-cart">Add to Cart</button>
   //         </div>
   //     </div>
+  
   let singleProduct = document.createElement("div");
   singleProduct.classList.add("single-product");
   let buyImgProduct = document.createElement("img");
@@ -101,16 +101,71 @@ function singleProductBuy(product: Store) {
   let addToCart = document.createElement("button");
   addToCart.classList.add("add-to-cart");
   addToCart.innerText = "Add to Cart";
-  nameAndButton.append(productTitle, productPrice, addToCart);
+  let backButton = document.createElement("button");
+  backButton.classList.add("back-button");
+  backButton.innerText = "Back";
+  backButton.addEventListener("click", () => {
+    if(container==null) return;
+    container.innerHTML = "";
+    render();
+  });
+
+  nameAndButton.append(productTitle, productPrice, addToCart, backButton);
   singleProduct.append(buyImgProduct, nameAndButton);
 
   container.append(singleProduct);
+}
+
+// !crearting the search bar
+// function createSearchBar() {
+//   let main = document.querySelector("main");
+//   if (main == null) return;
+//   main.innerHTML = "";
+
+//   let searchBar = document.createElement("div");
+//   searchBar.classList.add("search-bar");
+//   let h1Element = document.createElement("h1");
+//   h1Element.innerText = "Search for your favorite products";
+//   let searchInput = document.createElement("input");
+//   searchInput.classList.add("search-input");
+//   searchInput.placeholder = "Search";
+//   let searchButton = document.createElement("button");
+//   searchButton.classList.add("search-button");
+//   searchButton.innerText = "Search";
+//   searchBar.append(h1Element,searchInput, searchButton);
+
+//   main.append(searchBar);
+// }
+function renderSearchbar() {
+  let searchIcon = document.querySelector(".magnify-glass");
+
+  let main = document.querySelector("main");
+  // main.innerHTML = "";
+
+  searchIcon.addEventListener("click", () => {
+    let searchBar = document.createElement("div");
+    searchBar.classList.add("active-search-bar");
+    searchBar.classList.add("search-bar");
+    let h1Element = document.createElement("h1");
+    h1Element.innerText = "Search for your favorite products";
+    let searchInput = document.createElement("input");
+    searchInput.classList.add("search-input");
+    searchInput.placeholder = "Search";
+    let searchButton = document.createElement("button");
+    searchButton.classList.add("search-button");
+    searchButton.innerText = "Search";
+    searchBar.append(h1Element, searchInput, searchButton);
+
+    main.append(searchBar);
+  })
 }
 
 function render() {
   let products = document.querySelector(".products");
   if (products == null) return;
   products.innerHTML = "";
+  
+  renderSearchbar();
 
   for (let products of state.store) {
     createProduct(products);
